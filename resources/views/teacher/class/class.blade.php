578a2586247f9b769a663da7e68ebbe09f00fc93
Jericho <jerichobantiquete@gmail.com>
2022-10-18 00:00:22 +0800

    restyle teacher class

--- resources/views/teacher/class/class.blade.php
+++ resources/views/teacher/class/class.blade.php
@@ -1,25 +1,15 @@
-<div
-    class="col-span-6 sm:col-span-3 lg:col-span-2 relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl  sm:my-8 sm:w-full sm:max-w-lg">
-    <div class="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-        <div class="sm:flex sm:items-start">
-            <div class="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                <h3 class="text-lg font-medium leading-6 text-gray-900" id="modal-title">
-                    {{ $class->class_name }}
-                </h3>
-                @if ($class->class_details)
-                <div class="mt-2">
-                    <p class="text-sm text-gray-500">
-                        {{ $class->class_details }}
-                    </p>
-                </div>
-                @endif
-            </div>
+<div class="col-span-6 sm:col-span-3 lg:col-span-2 w-full  bg-white rounded-lg border border-gray-200 shadow-md ">
+    <div class="flex flex-col items-center py-10">
+        <div class="mb-3 w-32 rounded-md flex items-center justify-center h-24 shadow-lg bg-slate-700 text-white">
+            {{ substr($class->class_name, 0, 1) }}
         </div>
-    </div>
-    <div class="bg-gray-50 px-4 py-3  sm:px-6">
-        <a href=""
-            class="mt-3 inline-flex w-full justify-center rounded-md border border-gray-300 bg-indigo-700  text-white px-4 py-2 text-base font-medium shadow-sm hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-            View Class
-        </a>
+        <h5 class="mb-1 text-xl font-medium text-gray-900 ">{{ $class->class_name }}</h5>
+        <span class="text-sm text-gray-500">{{ $class->user->name }}</span>
+        <div class="flex mt-4 space-x-3 md:mt-6">
+            <a href=""
+                class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-indigo-700 rounded-lg hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 ">
+                View Class
+            </a>
+        </div>
     </div>
 </div>
--- resources/views/teacher/class/index.blade.php
+++ resources/views/teacher/class/index.blade.php
@@ -4,16 +4,19 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white flex items-center justify-between border-b border-gray-200">
                     <div>
-                        <h2 class="text-2xl font-bold">Class</h2>
+                        <h2 class="text-2xl font-bold">My Class</h2>
                     </div>
                     <div>
-                        <a class="bg-slate-700 text-white hover:bg-slate-800 px-6 py-2 rounded"
-                            href="{{ route('class.create') }}">
-                            Add Class
+                        <a href="{{ route('class.create') }}"
+                            class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-slate-700 rounded hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300">
+                            <svg class="w-3 h-3 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                <path
+                                    d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                            </svg>
+                            <span class="xs:block text-sm ml-2">Create Class</span>
                         </a>
                     </div>
                 </div>
-
 
                 {{-- --}}
                 <div class="p-6 grid grid-cols-6 gap-6">

7a2d48bb7cfc2f10e7ea7ee0d6d2afa54dad3269
Jericho <jerichobantiquete@gmail.com>
2022-12-05 23:38:31 +0800

    prevent student to pass activity if the deadline is met

--- resources/views/user/classes/activities/activity-details.blade.php
+++ resources/views/user/classes/activities/activity-details.blade.php
@@ -11,17 +11,21 @@
                         @endif
                         <p class="mt-1 max-w-2xl text-sm text-gray-500"></p>
                     </div>
-                    @include('user.classes.activities.upload-answer')
-                    {{-- <div>
-                        <a href="#"
-                            class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-slate-700 rounded hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300">
-                            <svg class="w-3 h-3 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
-                                <path
-                                    d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                            </svg>
-                            <span class="xs:block text-sm ml-2">Upload Answer</span>
-                        </a>
-                    </div> --}}
+                    @if ($activity->due_date <= date('Y-m-d')) <button x-on:click="createOpen = true"
+                        class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300">
+                        <span class="xs:block text-sm ml-2">Deadline Ended</span>
+                        </button>
+                        @else
+                        @include('user.classes.activities.upload-answer') @endif {{-- <div>
+                            <a href="#"
+                                class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-slate-700 rounded hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300">
+                                <svg class="w-3 h-3 fill-current opacity-50 shrink-0" viewBox="0 0 16 16">
+                                    <path
+                                        d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+                                </svg>
+                                <span class="xs:block text-sm ml-2">Upload Answer</span>
+                            </a>
+                        </div> --}}
                 </div>
                 <div class="border-t border-gray-200">
                     <dl>
